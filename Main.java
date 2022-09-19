09d710c0b1420ea6a20eb86d08482fe68bd023a6
şerif enes atçı <95710653+serifenes@users.noreply.github.com>
2022-09-19 18:57:48 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[]args){
-        System.out.println("-----------------------");
-        System.out.println("Herkese merhaba. Nasilsiniz ?");
-        System.out.println("Java ogreniyoruzzz");
-        System.out.println("-----------------------");
-    }
-}
